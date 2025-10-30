05e9169634e5c8e8eeb42233d6cd09a0a0b39019
saveliyu <uljanoff.sav@yandex.ru>
2025-10-30 18:05:50 +0700

    setup django project

---
+++ courses/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+courses_base = [
+    {'id': 1, 'category': 2, 'title': 'Python', 'description': 'Python course with OOP for beginners'},
+    {'id': 2, 'category': 2, 'title': 'Django', 'description': 'Backend web development with Django framework'},
+    {'id': 3, 'category': 3, 'title': 'SQL', 'description': 'Learn to work with databases and write complex queries'},
+]
+
+cats = [
+    {'id': 1, 'name': 'frontend'},
+    {'id': 2, 'name': 'backend'},
+    {'id': 3, 'name': 'database'},
+    {'id': 4, 'name': 'machine learning'},
+]
+
+def index(request):
+    context = {'courses': courses_base, 'cats': cats, 'selected_cat': 0}
+    return render(request, 'courses/index.html', context)
+
+
+def course(request, course_id):
+    context = {'course': courses_base[course_id - 1]}
+    return render(request, 'courses/course.html', context)
+
+def category(request, category_id):
+    context = {'courses': courses_base, 'cats': cats, 'selected_cat': category_id}
+    return render(request, 'courses/index.html', context)
